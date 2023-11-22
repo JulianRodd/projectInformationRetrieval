8db9ff28040a4b9b7f9d641e501fb8f3cb73640c
Julian Roddeman <Julianrdmn@gmail.com>
2023-11-22 14:33:45 +0100

    peft bert and bert and tensorboard

--- project/main.py
+++ project/main.py
@@ -1,9 +1,10 @@
 from models.model_handler import Model
 from data_loading.test_data_loader_imdb import load_imdb_data
 from torch.utils.data import DataLoader
+from models.peft_bert import PEFTBERTClassifier
+from models.bert import BERTClassifier
 
-
-bert = Model()
+bert = Model(PEFTBERTClassifier, device = "mps", run_name="peftbert")
 
 train_dataset, val_dataset = load_imdb_data(bert.max_length, bert.tokenizer)
 train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
--- project/models/bert.py
+++ project/models/bert.py
@@ -4,7 +4,7 @@ from transformers import BertModel
 class BERTClassifier(nn.Module):
     def __init__(self, bert_model_name, num_classes):
         super(BERTClassifier, self).__init__()
-        self.bert = BertModel.from_pretrained(bert_model_name)
+        self.bert = BertModel.from_pretrained(bert_model_name,)
         self.dropout = nn.Dropout(0.1)
         self.fc = nn.Linear(self.bert.config.hidden_size, num_classes)
 
